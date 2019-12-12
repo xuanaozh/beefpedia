63fcdab507fa976cb962900b64c34797568caebd
Shawn Zhang <xuanaozh@newschool.edu>
2019-12-12 01:10:21 -0500

    1

---
+++ build/precache-manifest.9b4294749c91e3bba9edae98f621dbb5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7012b7c5ebd45917e455bb73b9c81be2",
+    "url": "/beefpedia/index.html"
+  },
+  {
+    "revision": "b70447478e9178e4d557",
+    "url": "/beefpedia/static/css/main.2223863d.chunk.css"
+  },
+  {
+    "revision": "61782cb364836e08cd62",
+    "url": "/beefpedia/static/js/2.98169dfb.chunk.js"
+  },
+  {
+    "revision": "b70447478e9178e4d557",
+    "url": "/beefpedia/static/js/main.04f3c9ec.chunk.js"
+  },
+  {
+    "revision": "04a290a61cc7b69fdc51",
+    "url": "/beefpedia/static/js/runtime-main.da9b5309.js"
+  },
+  {
+    "revision": "098a8431c9a06d90c10e0a2797127f38",
+    "url": "/beefpedia/static/media/backrib.098a8431.png"
+  },
+  {
+    "revision": "7bd77d2452f638d6fc37ec3a9452adbf",
+    "url": "/beefpedia/static/media/beef_cuts.7bd77d24.svg"
+  },
+  {
+    "revision": "5339aad0daf822fa9649817e0ccb672c",
+    "url": "/beefpedia/static/media/bottomrump.5339aad0.png"
+  },
+  {
+    "revision": "d86fe08d139fe5111f50c55729017af1",
+    "url": "/beefpedia/static/media/brisket.d86fe08d.png"
+  },
+  {
+    "revision": "c8cb84c2922429f140ccb3156cdec71c",
+    "url": "/beefpedia/static/media/chuckblade.c8cb84c2.png"
+  },
+  {
+    "revision": "a8448196a8aaccfa4febfd8f4fdb69af",
+    "url": "/beefpedia/static/media/chuckroast.a8448196.png"
+  },
+  {
+    "revision": "38b1278fdf76e840363ec0c4a3f5c413",
+    "url": "/beefpedia/static/media/clod.38b1278f.png"
+  },
+  {
+    "revision": "66b59d8b3f7168ecec9c520a582394b4",
+    "url": "/beefpedia/static/media/flank.66b59d8b.png"
+  },
+  {
+    "revision": "dcc3380d3deb7cab9a0d34da627b09d8",
+    "url": "/beefpedia/static/media/halfbrisket.dcc3380d.png"
+  },
+  {
+    "revision": "117047643bd044553b0a75c473dc246a",
+    "url": "/beefpedia/static/media/knuckle.11704764.png"
+  },
+  {
+    "revision": "35dbc361c8df337f4b294afb9bf68c2a",
+    "url": "/beefpedia/static/media/leg.35dbc361.png"
+  },
+  {
+    "revision": "25a75c16788b0c27639467edd56d2a7c",
+    "url": "/beefpedia/static/media/ribeye.25a75c16.png"
+  },
+  {
+    "revision": "139a0c06562771da055f82fc2ee9ba31",
+    "url": "/beefpedia/static/media/roundsteak.139a0c06.png"
+  },
+  {
+    "revision": "f215e8d4afeda6354738db0a390651da",
+    "url": "/beefpedia/static/media/shank.f215e8d4.png"
+  },
+  {
+    "revision": "65f24bab5e6fd12dff4033e91eaa07a2",
+    "url": "/beefpedia/static/media/tbone.65f24bab.png"
+  },
+  {
+    "revision": "064cefeeba17730fb299fbeb42f2fc6f",
+    "url": "/beefpedia/static/media/tiproast.064cefee.png"
+  },
+  {
+    "revision": "c997e67d3946095ed5147e6141b2d441",
+    "url": "/beefpedia/static/media/tipsteak.c997e67d.png"
+  },
+  {
+    "revision": "cb1d06ef2ca717de18a4397bb7250b2f",
+    "url": "/beefpedia/static/media/toploin.cb1d06ef.png"
+  },
+  {
+    "revision": "3ad7a987cb63e0554ae88c57acdadcbb",
+    "url": "/beefpedia/static/media/toproast.3ad7a987.png"
+  },
+  {
+    "revision": "c7243e46f84f1885c4ee713096523860",
+    "url": "/beefpedia/static/media/toprump.c7243e46.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.742b3476a4ddee2c85dad414b7a41910.js"
+  "/beefpedia/precache-manifest.9b4294749c91e3bba9edae98f621dbb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beefpedia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
